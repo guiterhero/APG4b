12d1b6d362ee6b9e88de7e66b4fb10de59c1bdaa
guitarhero <ayumu_note@outlook.jp>
2023-03-19 21:37:38 +0900

    Create EX8

---
+++ EX8.py
@@ -0,0 +1,13 @@
+pattern = int(input())
+
+if pattern == 1:
+    price = int(input())
+    number = int(input())
+    print(number * price)
+
+elif pattern == 2:
+    text = input()
+    price = int(input())
+    number = int(input())
+    print(f"{text}!")
+    print(number * price)
